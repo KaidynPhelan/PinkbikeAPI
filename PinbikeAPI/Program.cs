1020b1e8ac93018ae98af27ea4ad8a80d3f28e79
KaidynPhelan <kaidyn.phelan08@gmail.com>
2022-06-28 14:37:39 +1000

    published to Azure and updated CORS to connect to Frontend

--- PinbikeAPI/Program.cs
+++ PinbikeAPI/Program.cs
@@ -3,9 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+
 
 app.UseAuthorization();
 
